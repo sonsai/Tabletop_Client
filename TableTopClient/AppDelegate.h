33bd92c59ae7d8494c84c99b85ddfeced57fcbc1
student <student@student-no-MacBook-Pro-3.local>
2014-10-13 16:37:48 +0900

    Initial Commit

---
+++ TableTopClient/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TableTopClient
+//
+//  Created by student on 14/10/13.
+//  Copyright (c) 2014年 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableTopClient/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TableTopClient
+//
+//  Created by student on 14/10/13.
+//  Copyright (c) 2014年 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
